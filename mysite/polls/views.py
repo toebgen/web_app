b835735bf6c2dcaca194f83e17d9faed399b75b6
Tobias Roth <tobias.roth.tr@gmail.com>
2019-04-09 09:39:04 +0200

    Raise Http404 if requested question_id is not available.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,4 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
-#from django.template import loader
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 
 from .models import Question
@@ -14,7 +12,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're Looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Questoin does not exist")
+    return render(request, 'polls/detail.html', { 'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
