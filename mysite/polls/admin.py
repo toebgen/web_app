b444da556412d8edf24dd6875e0ceda0ca2ebafe
Tobias Roth <tobias.roth.tr@gmail.com>
2019-04-11 15:56:14 +0200

    Improve Admin site for Question and Choice.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-# Register your models here.
 
 from .models import Choice, Question
 
-admin.site.register(Choice)
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               { 'fields': ['question_text']}),
+        ('Date information', { 'fields': ['pub_date'],
+                                         'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
