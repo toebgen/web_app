2cf1e93650f506e363faf595bb32a69748008752
Tobias Roth <tobias.roth.tr@gmail.com>
2019-04-12 16:32:08 +0200

    Add player view and template.

--- mysite/kicker/urls.py
+++ mysite/kicker/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+# Namespacing the URL names
+app_name = 'kicker'
+
 urlpatterns = [
+    # ex: /kicker/
     path('', views.index, name='index'),
+    # ex: /kicker/3/
+    path('<int:player_id>/', views.detail, name='detail'),
 ]
--- mysite/kicker/views.py
+++ mysite/kicker/views.py
@@ -1,6 +1,16 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+
+from .models import Player
 
 
 def index(request):
-    return HttpResponse("Player view!")
+    latest_player_list =  Player.objects.order_by('-join_date')[:5]
+    context = {'latest_player_list': latest_player_list}
+    output = ', '.join([p.name for p in latest_player_list])
+    return render(request, 'kicker/index.html', context)
+
+
+def detail(request, player_id):
+    player = get_object_or_404(Player, pk=player_id)
+    return render(request, 'kicker/player.html', {'player': player})
