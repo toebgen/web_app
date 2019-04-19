00558df16ec47b26df08570835fa19bfcebe1e04
Tobias Roth <tobias.roth.tr@gmail.com>
2019-04-19 18:21:33 +0200

    feat: Make Game and Player IDs visible on admin site.

--- mysite/kicker/admin.py
+++ mysite/kicker/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 from .models import Game, Player
 
 
-admin.site.register(Game)
-admin.site.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+class GameAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(Player, PlayerAdmin)
